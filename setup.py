755332a0469e337cefe8e25382b9cf1faf92e19f
rodneyshupe <rodney@shupe.ca>
2020-07-18 23:02:26 -0700

    added missing find_packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
